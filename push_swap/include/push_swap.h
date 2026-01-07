4d3c2cec9a4e25c3f3137213e991ed44409e5c18
Luis Quental Almeida <luis.quental.almeida@gmail.com>
2026-01-07 23:54:53 +0000

    Update naming convention for readability, correct mistake in s_stacks struct and add prototypes for stack operations.

--- push_swap/include/push_swap.h
+++ push_swap/include/push_swap.h
@@ -6,4 +6,39 @@
 # include <stdlib.h>
 # include <limits.h>
 
+/******************** DATA STRUCTURES: ********************/
+typedef struct s_node
+{
+	int				value;
+	int				index;
+	struct s_node	*next;
+	struct s_node	*prev;
+}	t_node;
+
+typedef struct s_stack
+{
+	int		size;
+	t_node	*top;
+	t_node	*bot;
+}	t_stack;
+
+typedef struct s_stacks
+{
+	t_stack	a;
+	t_stack	b;
+}	t_stacks;
+
+/******************** STACK OPERATIONS: *******************/
+void	swap_a(t_stacks *stacks, int print);
+void	swap_b(t_stacks *stacks, int print);
+void	swap_s(t_stacks *stacks, int print);
+void	push_a(t_stacks *stacks, int print);
+void	push_b(t_stacks *stacks, int print);
+void	rot_a(t_stacks *stacks, int print);
+void	rot_b(t_stacks *stacks, int print);
+void	rot_s(t_stacks *stacks, int print);
+void	rrot_a(t_stacks *stacks, int print);
+void	rrot_b(t_stacks *stacks, int print);
+void	rrot_s(t_stacks *stacks, int print);
+
 #endif
