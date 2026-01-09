8c764682eef2b0bc9d611a1903d000dd620c7b1c
Luis Quental Almeida <luis.quental.almeida@gmail.com>
2026-01-09 02:17:22 +0000

    Create node_utils.c file and move functions node_init, stack_push_top, stack_push_bot, stack_pop_top and stack_pop_bot to this file, since they are operations to create nodes, insert or remove notes from stacks.

---
+++ push_swap/src/node_utils.c
@@ -0,0 +1,79 @@
+#include "push_swap.h"
+
+t_node	*node_init(int value)
+{
+	t_node	*node;
+
+	node = (t_node *)malloc(sizeof(t_node));
+	if (!node)
+		return (NULL);
+	node->value = value;
+	node->index = -1;
+	node->next = NULL;
+	node->prev = NULL;
+	return (node);
+}
+
+void	stack_push_top(t_stack *stack, t_node *node)
+{
+	if (!stack || !node)
+		return ;
+	node->prev = NULL;
+	node->next = stack->top;
+	if (stack->top)
+		stack->top->prev = node;
+	stack->top = node;
+	if (stack->bot == NULL)
+		stack->bot = node;
+	stack->size++;
+}
+
+void	stack_push_bot(t_stack *stack, t_node *node)
+{
+	if (!stack || !node)
+		return ;
+	node->next = NULL;
+	node->prev = stack->bot;
+	if (stack->bot)
+		stack->bot->next = node;
+	stack->bot = node;
+	if (stack->top == NULL)
+		stack->top = node;
+	stack->size++;
+}
+
+t_node	*stack_pop_top(t_stack *stack)
+{
+	t_node	*node;
+
+	if (!stack || stack->size == 0)
+		return (NULL);
+	node = stack->top;
+	stack->top = node->next;
+	if (stack->top)
+		stack->top->prev = NULL;
+	else
+		stack->bot = NULL;
+	node->next = NULL;
+	node->prev = NULL;
+	stack->size--;
+	return (node);
+}
+
+t_node	*stack_pop_bot(t_stack *stack)
+{
+	t_node	*node;
+
+	if (stack || stack->size == 0)
+		return (NULL);
+	node = stack->bot;
+	stack->bot = node->prev;
+	if (stack->bot)
+		stack->bot->next = NULL;
+	else
+		stack->top = NULL;
+	node->next = NULL;
+	node->prev = NULL;
+	stack->size--;
+	return (node);
+}
--- push_swap/src/push_ops.c
+++ push_swap/src/push_ops.c
@@ -1,37 +1,5 @@
 #include "push_swap.h"
 
-t_node	*stack_pop_top(t_stack *stack)
-{
-	t_node	*node;
-
-	if (!stack || stack->size == 0)
-		return (NULL);
-	node = stack->top;
-	stack->top = node->next;
-	if (stack->top)
-		stack->top->prev = NULL;
-	else
-		stack->bot = NULL;
-	node->next = NULL;
-	node->prev = NULL;
-	stack->size--;
-	return (node);
-}
-
-void	stack_push_top(t_stack *stack, t_node *node)
-{
-	if (!stack || !node)
-		return ;
-	node->prev = NULL;
-	node->next = stack->top;
-	if (stack->top)
-		stack->top->prev = node;
-	stack->top = node;
-	if (stack->bot == NULL)
-		stack->bot = node;
-	stack->size++;
-}
-
 void	push_a(t_stacks *stacks, int print)
 {
 	t_node	*node;
