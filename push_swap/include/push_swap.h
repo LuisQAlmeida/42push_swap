0b9ccc31ee14b16967a8850ed593e67c40c761c4
Luis Quental Almeida <luis.quental.almeida@gmail.com>
2026-01-09 16:34:00 +0000

    Re-organize prototypes section - add section for NODE UTILS.

--- push_swap/include/push_swap.h
+++ push_swap/include/push_swap.h
@@ -31,11 +31,13 @@ typedef struct s_stacks
 /******************** STACK UTILS *************************/
 void	stack_init(t_stack *stack);
 void	stack_clear(t_stack *stack);
+
+/******************** NODE UTILS **************************/
+t_node  *node_init(int value);
+t_node  *stack_pop_top(t_stack *stack);
+t_node  *stack_pop_bot(t_stack *stack);
 void	stack_push_top(t_stack *stack, t_node *node);
 void	stack_push_bot(t_stack *stack, t_node *node);
-t_node	*stack_pop_top(t_stack *stack);
-t_node	*stack_pop_bot(t_stack *stack);
-t_node	*node_init(int value);
 
 /******************** STACK OPERATIONS: *******************/
 void	swap_a(t_stacks *stacks, int print);
